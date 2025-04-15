da0ecb111dce2e6601724bb370c7289cee4b67e9
Murilo Weishaupt <muriloweishaupt@gmail.com>
2025-04-15 00:21:06 -0300

    Organizados componentes da interface

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import requests
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
 def pegar_cotacoes():
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-
     requisicao_dic = requisicao.json()
 
     cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
@@ -11,24 +11,34 @@ def pegar_cotacoes():
     cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
 
     texto = f'''
-        Dólar: {cotacao_dolar}
-        Euro: {cotacao_euro}
-        Bitcoin: {cotacao_btc}
-    '''
-
+Dólar: R$ {float(cotacao_dolar):.2f}
+Euro: € {float(cotacao_euro):.2f}
+Bitcoin: ₿ {float(cotacao_btc):.2f}
+'''
     texto_cotacoes["text"] = texto
 
 
-janela = Tk()
+janela = tk.Tk()
+janela.title("💰 Cotação Atual das Moedas")
+janela.geometry("350x250")
+janela.configure(bg="#f2f2f2")
 
-janela.title("Cotação atual das Moedas")
-texto_orientacao = Label(janela, text="Clique no botão para ver as cotações das moedas")
-texto_orientacao.grid(column=1, row=0, padx=10, pady=10)
 
-botao = Button(janela, text="Buscar cotações Dólar/Euro/BTC", command=pegar_cotacoes)
-botao.grid(column=1, row=1,  padx=10, pady=10)
+style = ttk.Style()
+style.theme_use("clam")
+style.configure("TButton", font=("Segoe UI", 10), padding=6)
+style.configure("TLabel", font=("Segoe UI", 11), background="#f2f2f2")
 
-texto_cotacoes = Label(janela, text="")
-texto_cotacoes.grid(column=1, row=2,  padx=10, pady=10)
+
+texto_orientacao = ttk.Label(janela, text="Clique no botão para ver as cotações:")
+texto_orientacao.pack(pady=(20, 10))
+
+
+botao = ttk.Button(janela, text="🔍 Buscar cotações", command=pegar_cotacoes)
+botao.pack(pady=10)
+
+
+texto_cotacoes = ttk.Label(janela, text="", font=("Consolas", 12), justify="center")
+texto_cotacoes.pack(pady=20)
 
 janela.mainloop()
